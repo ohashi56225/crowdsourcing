13a8b29f048ee557be6cbe619f34719537e81840
ohashi56225 <ohashi.atsumoto.c0@s.mail.nagoya-u.ac.jp>
2024-12-30 19:38:41 +0900

    Add sample audio

--- evalset2audiofiles.js
+++ evalset2audiofiles.js
@@ -1,11 +1,11 @@
 evalSet2audioFiles = {
     test1: [
-        "audiofiles/100.wav",
-        "audiofiles/200.wav",
-        "audiofiles/300.wav",
-        "audiofiles/400.wav",
-        "audiofiles/500.wav",
-        "audiofiles/600.wav",
-        "audiofiles/700.wav"
+        "audio/100.wav",
+        "audio/200.wav",
+        "audio/300.wav",
+        "audio/400.wav",
+        "audio/500.wav",
+        "audio/600.wav",
+        "audio/700.wav"
     ]
 };
