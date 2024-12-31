93c7d94a1d817acbd085f67e9e5ed73ff64108b5
ohashi56225 <ohashi.atsumoto.c0@s.mail.nagoya-u.ac.jp>
2024-12-31 13:44:44 +0900

    Add test tasks

--- evalset2audiofiles.js
+++ evalset2audiofiles.js
@@ -1,11 +1,122 @@
 evalSet2audioFiles = {
-    test1: [
-        "audio/100.wav",
-        "audio/200.wav",
-        "audio/300.wav",
-        "audio/400.wav",
-        "audio/500.wav",
-        "audio/600.wav",
-        "audio/700.wav"
+    "2qb69wrg": [
+        "audio/dse0-478.wav",
+        "audio/osfv-227.wav",
+        "audio/dse0-307.wav",
+        "audio/079n-492.wav",
+        "audio/dse0-0.wav",
+        "audio/079n-682.wav",
+        "audio/osfv-328.wav",
+        "audio/079n-328.wav",
+        "audio/osfv-682.wav",
+        "audio/34to-0.wav"
+    ],
+    "k0b5h8ko": [
+        "audio/osfv-492.wav",
+        "audio/dse0-487.wav",
+        "audio/dse0-682.wav",
+        "audio/079n-227.wav",
+        "audio/079n-0.wav",
+        "audio/osfv-69.wav",
+        "audio/osfv-508.wav",
+        "audio/34to-1.wav",
+        "audio/079n-492.wav",
+        "audio/dse0-492.wav"
+    ],
+    "12781obr": [
+        "audio/079n-492.wav",
+        "audio/osfv-478.wav",
+        "audio/dse0-227.wav",
+        "audio/079n-682.wav",
+        "audio/dse0-682.wav",
+        "audio/osfv-0.wav",
+        "audio/34to-4.wav",
+        "audio/dse0-478.wav",
+        "audio/079n-508.wav",
+        "audio/osfv-492.wav"
+    ],
+    "b89vv973": [
+        "audio/079n-0.wav",
+        "audio/079n-227.wav",
+        "audio/osfv-508.wav",
+        "audio/dse0-487.wav",
+        "audio/dse0-227.wav",
+        "audio/079n-69.wav",
+        "audio/34to-2.wav",
+        "audio/dse0-508.wav",
+        "audio/osfv-487.wav",
+        "audio/osfv-307.wav"
+    ],
+    "tktiozzd": [
+        "audio/dse0-492.wav",
+        "audio/079n-69.wav",
+        "audio/osfv-328.wav",
+        "audio/079n-487.wav",
+        "audio/dse0-328.wav",
+        "audio/osfv-0.wav",
+        "audio/osfv-508.wav",
+        "audio/34to-4.wav",
+        "audio/dse0-478.wav",
+        "audio/079n-508.wav"
+    ],
+    "v8uufgjg": [
+        "audio/079n-328.wav",
+        "audio/dse0-0.wav",
+        "audio/34to-2.wav",
+        "audio/osfv-328.wav",
+        "audio/dse0-328.wav",
+        "audio/079n-478.wav",
+        "audio/osfv-682.wav",
+        "audio/dse0-69.wav",
+        "audio/osfv-69.wav",
+        "audio/079n-307.wav"
+    ],
+    "smsqm483": [
+        "audio/osfv-492.wav",
+        "audio/079n-508.wav",
+        "audio/osfv-0.wav",
+        "audio/34to-0.wav",
+        "audio/dse0-508.wav",
+        "audio/dse0-69.wav",
+        "audio/dse0-0.wav",
+        "audio/osfv-682.wav",
+        "audio/079n-307.wav",
+        "audio/079n-227.wav"
+    ],
+    "cchlrde1": [
+        "audio/079n-0.wav",
+        "audio/dse0-227.wav",
+        "audio/osfv-69.wav",
+        "audio/079n-69.wav",
+        "audio/dse0-307.wav",
+        "audio/dse0-492.wav",
+        "audio/osfv-487.wav",
+        "audio/34to-4.wav",
+        "audio/osfv-227.wav",
+        "audio/079n-487.wav"
+    ],
+    "z6n75ves": [
+        "audio/dse0-307.wav",
+        "audio/34to-2.wav",
+        "audio/osfv-478.wav",
+        "audio/osfv-307.wav",
+        "audio/dse0-69.wav",
+        "audio/osfv-227.wav",
+        "audio/079n-328.wav",
+        "audio/dse0-487.wav",
+        "audio/079n-682.wav",
+        "audio/079n-478.wav"
+    ],
+    "kha9bifn": [
+        "audio/34to-4.wav",
+        "audio/osfv-307.wav",
+        "audio/osfv-487.wav",
+        "audio/dse0-682.wav",
+        "audio/osfv-478.wav",
+        "audio/dse0-508.wav",
+        "audio/079n-307.wav",
+        "audio/dse0-328.wav",
+        "audio/079n-478.wav",
+        "audio/079n-487.wav"
     ]
-};
+}
